6f706c2e6d70a7a8fb5b9da19cff951fbc652375
dongxinhua <798616582@qq.com>
2021-11-23 20:46:47 +0800

    feat: yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,8 +14,8 @@ remote=$(git config remote.origin.url)
 echo 'remote is: '$remote
 
 # 新建一个发布的目录
-mkdir gh-pages-branch
-cd gh-pages-branch
+mkdir master-branch
+cd master-branch
 # 创建的一个新的仓库
 # 设置发布的用户名与邮箱
 git config --global user.email "$GH_EMAIL" >/dev/null 2>&1
@@ -27,13 +27,13 @@ echo 'email is: '$GH_EMAIL
 echo 'name is: '$GH_NAME
 echo 'sitesource is: '$siteSource
 
-# 切换gh-pages分支
-if git rev-parse --verify origin/gh-pages >/dev/null 2>&1; then
-  git checkout gh-pages
+# 切换master分支
+if git rev-parse --verify origin/master >/dev/null 2>&1; then
+  git checkout master
   # 删除掉旧的文件内容
   git rm -rf .
 else
-  git checkout --orphan gh-pages
+  git checkout --orphan master
 fi
 
 # 把构建好的文件目录给拷贝进来
@@ -46,9 +46,9 @@ git add -A
 # 添加一条提交内容
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # 推送文件
-git push --force --quiet origin gh-pages
+git push --force --quiet origin master
 # 资源回收，删除临时分支与目录
 cd ..
-rm -rf gh-pages-branch
+rm -rf master-branch
 
 echo "Finished Deployment!"
